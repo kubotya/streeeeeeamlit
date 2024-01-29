b222e097fcea8d4a92f3bfaa7e28bb8336b371ca
Diseive <104061331+kubotya@users.noreply.github.com>
2024-01-29 12:01:28 +0900

    model.py

--- model.py
+++ model.py
@@ -12,29 +12,29 @@ img_size = 28
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 16, 3)
-        self.conv2 = nn.Conv2d(16, 32, 3)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 64, 3)
-        self.conv4 = nn.Conv2d(64, 128, 3)
-        self.bn2 = nn.BatchNorm2d(128)
+        self.conv1 = nn.Conv2d(1, 8, 3)
+        self.conv2 = nn.Conv2d(8, 16, 3)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv3 = nn.Conv2d(16, 32, 3)
+        self.conv4 = nn.Conv2d(32, 64, 3)
+        self.bn2 = nn.BatchNorm2d(64)
 
         self.pool = nn.MaxPool2d(2, 2)
         self.relu = nn.ReLU()
 
-        self.fc1 = nn.Linear(128*4*4, 512)
+        self.fc1 = nn.Linear(64*4*4, 256)
         self.dropout = nn.Dropout(p=0.5)
-        self.fc2 = nn.Linear(512, 10)
+        self.fc2 = nn.Linear(256, 10)
 
     def forward(self, x):
         x = self.relu(self.conv1(x))
         x = self.relu(self.bn1(self.conv2(x)))
         x = self.pool(x)
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.bn2(self.conv4(x)))
+        x = self.relu(self.conv3(x))
+        x = self.relu(self.bn2(self.conv4(x)))
         x = self.pool(x)
-        x = x.view(-1, 128*4*4)
-        x = F.relu(self.fc1(x))
+        x = x.view(-1, 64*4*4)
+        x = self.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
         return x
